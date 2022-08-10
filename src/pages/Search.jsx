5b17826c6b1f16c7076b35e71c1edca303c312e8
Raypcte <rayanevalak@gmail.com>
2022-08-10 00:13:22 -0300

    questao 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    botaozinho: true,
+    caractere: '',
+  }
+
+  musica = (event) => {
+    this.setState({
+      caractere: event.target.value,
+    });
+
+    if (event.target.value.length >= 2) {
+      this.setState({
+        botaozinho: false,
+      });
+    } else {
+      this.setState({
+        botaozinho: true,
+      });
+    }
+  }
+
   render() {
+    const { botaozinho, caractere } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          value={ caractere }
+          onChange={ this.musica }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ botaozinho }
+        >
+          Pesquisar
+        </button>
 
       </div>
     );
